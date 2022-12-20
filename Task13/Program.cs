1137b0a743fda811942709f4de3dbff981e109ff
Irina <liamina2207@gmail.com>
2022-12-21 00:07:56 +0300

    Solved Task 13

--- Task13/Program.cs
+++ Task13/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 13: Напишите программу, которая 
+// выводит третью цифру заданного числа или 
+// сообщает, что третьей цифры нет. 
+// Выполнить с помощью числовых операций 
+// (целочисленное деление, остаток от деления).
+
+// 645 -> 5
+
+// 78 -> третьей цифры нет
+
+// 32679 -> 6
+
+int number = new Random().Next();
+int count = number;
+if (number < 100)
+{
+    Console.WriteLine($"{number} -> третьей цифры нет");
+}
+else 
+{
+    while (count > 999)
+    {
+        count = count / 10;
+    }
+    int showThirdDigit = ShowThirdDigit(count);
+    Console.WriteLine($"{number} -> {showThirdDigit}");
+    int ShowThirdDigit(int num)
+    {
+        int result = num % 10;
+        return result;
+    }
+}
