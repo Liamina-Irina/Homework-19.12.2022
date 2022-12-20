9014811dce4244bb9aa6223c5e80aa3c2ca75e40
Irina <liamina2207@gmail.com>
2022-12-20 23:04:45 +0300

    Solved Task 10

--- Task10/Program.cs
+++ Task10/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 10: Напишите программу, которая 
+// принимает на вход трёхзначное число и 
+// на выходе показывает вторую цифру этого числа. 
+// Выполнить с помощью числовых операций 
+// (целочисленное деление, остаток от деления).
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+int number = new Random().Next(100, 1000);
+Console.WriteLine($"Случайное трехзначное число: {number}");
+
+int showSecondDigit = ShowSecondDigit(number);
+Console.WriteLine ($"{number} -> {showSecondDigit}");
+
+int ShowSecondDigit(int num)
+{
+    int result = (num / 10) % 10;
+    return result;
+}
